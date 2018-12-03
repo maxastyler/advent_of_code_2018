784a9c19ed221e94da3b8a1cb429fe3a548bccca
Max Tyler <maxastyler@gmail.com>
2018-12-03 12:46:15 +0000

    Fix some stuff

--- day_2/solution.rs
+++ day_2/solution.rs
@@ -2,12 +2,11 @@ fn main() {
     let input: Vec<_> = include_str!("./input.txt").lines().collect();
     let letter_counted = input.iter().map(|s| (0..26).map(move |c: u8| s.chars()
                                            .filter(move |&l| l==(c+97) as char)
-                                           .collect::<Vec<_>>()
-                                           .len())
+                                           .count())
                               .collect::<Vec<_>>());
     println!("Checksum: {}", 
-             letter_counted.clone().filter(|x| x.contains(&2)).collect::<Vec<_>>().len() *
-             letter_counted.clone().filter(|x| x.contains(&3)).collect::<Vec<_>>().len());
+             letter_counted.clone().filter(|x| x.contains(&2)).count() *
+             letter_counted.clone().filter(|x| x.contains(&3)).count());
 
     let repeated = input.iter()
                          .enumerate()
@@ -21,8 +20,7 @@ fn main() {
                                             .collect::<Vec<_>>())
                          .flatten();
 
-    let line_numbers: (usize, usize) = repeated.filter(|(x, _) | x==&25)
-        .map(|(_, x)| x).next().unwrap();
+    let line_numbers: (usize, usize) = repeated.filter_map(|(x, y)| if x==25 { Some(y) } else { None } ).next().unwrap();
 
     println!("Line number | Line\n{} | {}\n{} | {}", 
              line_numbers.0, input[line_numbers.0], line_numbers.1, input[line_numbers.1]);
@@ -30,7 +28,6 @@ fn main() {
     println!("Common letters: {}", input[line_numbers.0]
              .chars()
              .zip(input[line_numbers.1].chars())
-             .filter(|(a, b)| a==b)
-             .map(|a| a.0)
+             .filter_map(|(a, b)| if a==b { Some(a) } else { None })
              .collect::<String>())
 }
