c34f22849564e0ecf3bfd47f7cb4608454204c82
Max Tyler <maxastyler@gmail.com>
2018-12-04 20:06:16 +0000

    Remove dependencies, no regex any more

--- day_4/solution/src/main.rs
+++ day_4/solution/src/main.rs
@@ -1,48 +1,34 @@
-extern crate regex;
-
 use std::collections::HashMap;
 
-use regex::{Regex, RegexSet};
-
 fn main() {
-
     // PARSING STUFF
 
     let mut input = include_str!("/home/max/git/advent_of_code_2018/day_4/input.txt")
         .lines()
         .collect::<Vec<_>>();
     &input[..].sort();
-    let regex_set = RegexSet::new(&[
-        r"\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\] Guard #\d+ begins shift",
-        r"\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\] falls asleep",
-        r"\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\] wakes up",
-    ])
-    .unwrap();
-    let id_regex =
-        Regex::new(r"\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\] Guard #(\d+) begins shift").unwrap();
-    let time_regex = Regex::new(r"\[\d{4}-\d{2}-\d{2} \d{2}:(\d{2})\] [[:ascii:]]").unwrap();
+
     let mut elves: HashMap<u16, Vec<u8>> = HashMap::new();
     let mut current: u16 = 0;
     let mut time: u8 = 0;
 
-    input
-        .iter()
-        .for_each(|x| match regex_set.matches(x).into_iter().next().unwrap() {
-            0 => {
-                current = id_regex.captures(x).unwrap()[1].parse::<u16>().unwrap();
-                if !elves.contains_key(&current) {
-                    elves.insert(current, vec![0; 60]);
-                }
+    input.iter().for_each(|&x| match x.get(19..20).unwrap() {
+        "G" => {
+            current = x.split(" ").collect::<Vec<_>>()[3][1..]
+                .parse::<u16>()
+                .unwrap();
+            if !elves.contains_key(&current) {
+                elves.insert(current, vec![0; 60]);
             }
-            1 => {
-                time = time_regex.captures(x).unwrap()[1].parse::<u8>().unwrap();
-            }
-            _ => {
-                let current_times = elves.get_mut(&current).unwrap();
-                (time..time_regex.captures(x).unwrap()[1].parse::<u8>().unwrap())
-                    .for_each(|i| current_times[i as usize] += 1)
-            }
-        });
+        }
+        "f" => {
+            time = x[15..17].parse::<u8>().unwrap();
+        }
+        _ => {
+            let current_times = elves.get_mut(&current).unwrap();
+            (time..x[15..17].parse::<u8>().unwrap()).for_each(|i| current_times[i as usize] += 1)
+        }
+    });
 
     // FINDING THE SLEEPY ELVES LOGIC
 
