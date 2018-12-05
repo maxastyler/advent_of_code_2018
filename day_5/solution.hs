e50469baf0dff7bfc2c0426f4bfa9ed60fa19810
Max Tyler <maxastyler@gmail.com>
2018-12-05 12:08:07 +0000

    Add solution

--- day_5/solution.hs
+++ day_5/solution.hs
@@ -3,8 +3,8 @@ import Data.Char
 main = do
   file_str <- readFile "input.txt"
   let filtered = filter (/='\n') file_str
-  print $ length $ react_iteratively filtered
-  print $ minimum [length $ react_iteratively $ filter (\x -> toLower x /= l) filtered | l <- ['a'..'z']]
+  print $ length $ react_foldr filtered
+  print $ minimum [length $ react_foldr $ filter (\x -> toLower x /= l) filtered | l <- ['a'..'z']]
 
 react_elements (a:rest@(b:c)) | same_letter && a /= b = react_elements c
                               | otherwise = a:(react_elements rest)
@@ -14,3 +14,8 @@ react_elements a = a
 react_iteratively s | s == s' = s
                     | otherwise = react_iteratively s'
                     where s' = react_elements s
+
+react_foldr :: [Char] -> [Char]
+react_foldr = foldr react ""
+  where react a (b:c) | a /= b && toUpper a == toUpper b = c
+        react a c = a:c
